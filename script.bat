bd50c9c738e8d50aeb4eff6b1793de4978fd5032
Walyson Scarazzati da Silva <walyson.scarazzati@gmail.com>
2023-11-15 15:44:50 -0300

    Finalização projeto 1 do bootcamp

---
+++ script.bat
@@ -0,0 +1,17 @@
+echo "Criando as imangens....."
+
+docker build -t walysonsilva/projeto-backend:1.0 backend/.
+docker build -t walysonsilva/projeto-database:1.0 database/.
+
+echo "Realizando o push das imagens..."
+
+docker push walysonsilva/projeto-backend:1.0
+docker push walysonsilva/projeto-database:1.0
+
+echo "Criando serviços no cluster Kubernetes.."
+
+kubectl apply -f ./services.yml
+
+echo "Criando os deployments..."
+
+kubectl apply -f ./deployment.yml
